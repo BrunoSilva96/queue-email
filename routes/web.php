f85d4044c123fcfe4bf107fcb162dc776e2879a4
Bruno Silva <brunosilvabrait@gmail.com>
2024-12-13 17:29:04 -0300

    config email params and routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\newLaravelTips;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('envio-email', function(){
-    return new \App\Mail\newLaravelTips();
+
+    $user = new stdClass();
+    $user->name = 'Bruno Silva';
+    $user->email = 'bruno@email.com';
+
+    //return new \App\Mail\newLaravelTips($user);
+    //\Illuminate\Support\Facades\Mail::send(new \App\Mail\newLaravelTips($user));
+    Mail::send(new newLaravelTips($user));
 });
